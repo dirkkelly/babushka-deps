a3fe0cdd9b2bc0c40a1e5e44ce088632d47c5bba
Dirk Kelly <d.kelly@murdoch.edu.au>
2011-08-25 13:09:04 +0800

    moved environment check out of client install

--- oracle.rb
+++ oracle.rb
@@ -1,11 +1,11 @@
 dep "oracle instant client" do
+  requires "oracle environment settings"
   met? {
     Dir.exists?  "/opt/oracle/instantclient"
     if File.exists? "/opt/oracle/instantclient/BASIC_README"
       shell("cat /opt/oracle/instantclient/BASIC_README").include?("32-bit")
       shell("cat /opt/oracle/instantclient/BASIC_README").include?("11.2.0.2.0")
     end
-    shell("cat ~/.bashrc").include?("ORACLE_BASE=/opt/oracle/instantclient")
   }
   meet {
     shell "mkdir -p ~/tmp"
@@ -18,9 +18,20 @@ dep "oracle instant client" do
     sudo  "rm -rf /opt/oracle/x86/instantclient_11_2"
     sudo  "mv instantclient_11_2 /opt/oracle/x86/"
     sudo  "ln -s /opt/oracle/x86/instantclient_11_2 /opt/oracle/instantclient"
+  }
+end
+
+dep "oracle environment settings" do
+  met? {
+    shell("cat ~/.bashrc").include?("ORACLE_BASE='/opt/oracle/instantclient'")
+    shell("cat ~/.bashrc").include?("ORACLE_HOME='/opt/oracle/instantclient'")
+    shell("cat ~/.bashrc").include?("LD_LIBRARY_PATH='/opt/oracle/instantclient'")
+    shell("cat ~/.bashrc").include?("TNS_ADMIN='/opt/oracle/instantclient'")
+  }
+  meet {
     shell "echo \"export ORACLE_BASE='/opt/oracle/instantclient'\" >> ~/.bashrc"
-    shell "echo \"export ORACLE_HOME=$ORACLE_BASE\" >> ~/.bashrc"
-    shell "echo \"export LD_LIBRARY_PATH=$ORACLE_BASE\" >> ~/.bashrc"
-    shell "echo \"export TNS_ADMIN=$ORACLE_BASE\" >> ~/.bashrc"
+    shell "echo \"export ORACLE_HOME='$ORACLE_BASE'\" >> ~/.bashrc"
+    shell "echo \"export LD_LIBRARY_PATH='$ORACLE_BASE'\" >> ~/.bashrc"
+    shell "echo \"export TNS_ADMIN='$ORACLE_BASE'\" >> ~/.bashrc"
   }
 end
