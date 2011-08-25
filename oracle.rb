a99a17c3be73274f4a6b485e3bddfba81ae03b62
Dirk Kelly <d.kelly@murdoch.edu.au>
2011-08-25 12:23:05 +0800

    added platform to oracle stuff

--- oracle.rb
+++ oracle.rb
@@ -3,12 +3,12 @@ dep "oracle instant client" do
   meet {
     shell "mkdir ~/tmp"
     shell "cd ~/tmp"
-    shell "wget http://csapc1.murdoch.edu.au/babushka-deps/instantclient_11_2.tar"
-    shell "tar -xvf instantclient_11_2.tar"
-    shell "rm instantclient_11_2.tar"
-    sudo  "mkdir -p /opt/oracle/"
+    shell "wget http://csapc1.murdoch.edu.au/babushka-deps/instantclient_11_2-x86.tar"
+    shell "tar -xvf instantclient_11_2-x86.tar"
+    shell "rm instantclient_11_2x-86.tar"
+    sudo  "mkdir -p /opt/oracle/x86/"
     sudo  "rm /opt/oracle/instantclient"
-    sudo  "mv instantclient_11_2 /opt/oracle/"
-    sudo  "ln -s /opt/oracle/instantclient_11_2 /opt/oracle/instantclient"
+    sudo  "mv instantclient_11_2 /opt/oracle/x86/"
+    sudo  "ln -s /opt/oracle/x86/instantclient_11_2 /opt/oracle/instantclient"
   }
 end
